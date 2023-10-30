3c5babab405266fb5286e30ea76dc9dcacecfe85
Owen <ocotten24@basecampcodingacademy.org>
2023-10-30 08:33:16 -0500

    type annotations

--- hello-django/app/views.py
+++ hello-django/app/views.py
@@ -3,5 +3,5 @@ from django.http.response import HttpResponse
 from django.http.request import HttpRequest
 
 
-def hello_view(request: HttpRequest):
+def hello_view(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello World!")
--- hello-django/config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from app.views import hello_view
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("hello/", hello_view),
-]
